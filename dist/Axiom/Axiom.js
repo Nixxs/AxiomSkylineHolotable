b8e4561c6376de40bb8e2ac4e98c79aebaf15da6
Reuben Willson <rwillson@euclideonholographics.com>
2022-05-06 14:04:36 +1000

    setup project correctly

--- dist/Axiom/Axiom.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports", "./ProgramManager"], function (require, exports, ProgramManager_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.basePath = void 0;
-    exports.basePath = "\\\\192.168.1.19/d/C-ARMSAS/axiom/";
-    window.addEventListener("load", ProgramManager_1.programManager.Init);
-});
