5f59a3101e5618c24e52f780a88fe1cf36e1cfb7
David Wilton <dave_wilton@msn.com>
2022-05-17 14:21:07 +1000

    add verbs

---
+++ src/config/verbs.ts
@@ -0,0 +1,40 @@
+export const verbsConfig = {
+    verbs: [
+        {
+            verbName: "amplify",
+            verbType: ""
+        },
+        {
+            verbName: "block",
+            verbType: ""
+        },
+        {
+            verbName: "breach",
+            verbType: ""
+        },
+        {
+            verbName: "bypass",
+            verbType: ""
+        },
+        {
+            verbName: "canalise",
+            verbType: ""
+        },
+        {
+            verbName: "clear",
+            verbType: ""
+        },
+        {
+            verbName: "coerce",
+            verbType: ""
+        },
+        {
+            verbName: "contain",
+            verbType: ""
+        },
+        {
+            verbName: "control",
+            verbType: ""
+        },
+    ]
+}
