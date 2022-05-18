4551aec9a908755058fea3b355a699f346e40759
David Wilton <dave_wilton@msn.com>
2022-05-18 10:52:05 +1000

    attach labels

--- src/UIControls/MenuVerbs.ts
+++ src/UIControls/MenuVerbs.ts
@@ -8,7 +8,6 @@ export class MenuVerbs extends MenuPaging {
 
     createButton(name: string, icon: string, callback?: (id?: string) => void) {
         // override and return the button as we need to add them all at once
-        console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
         const groupId = ProgramManager.getInstance().getGroupID("buttons");
         const pos = sgWorld.Creator.CreatePosition(0, 0, 0.7, 3);
         const btn = new ButtonLabelled(name, pos, basePath + "ui/" + icon, groupId, callback);
