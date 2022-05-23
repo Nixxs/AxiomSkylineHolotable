cdce7fddea0d33d510a31d4bd69ce5663c5d9bd5
David Wilton <dave_wilton@msn.com>
2022-05-23 12:11:33 +1000

    fix back to nas

--- src/Axiom.ts
+++ src/Axiom.ts
@@ -4,8 +4,8 @@ declare const SGWorld: ISGWorld;
 declare const SessionManager: ISessionManager;
 export const sgWorld = SGWorld;
 export const sessionManager = SessionManager;
-// export const basePath = "\\\\Skyline_NAS/Data/C-ARMSAS/TableAPI/Axiom/";
-export const basePath = "C:/dev/Github/AxiomSkylineHolotable/dist/Axiom/";
+export const basePath = "\\\\Skyline_NAS/Data/C-ARMSAS/TableAPI/Axiom/";
+// export const basePath = "C:/dev/Github/AxiomSkylineHolotable/dist/Axiom/";
 
 if (document.readyState === 'complete')
   ProgramManager.getInstance().Init();
