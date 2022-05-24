3a8ce9d1efe550f77200e2db5df691bbeeffe195
Nicholas Chai <nicholas.chai@outook.com>
2022-05-24 19:15:16 +1000

    change the path back to to proper produciton path

--- src/Axiom.ts
+++ src/Axiom.ts
@@ -4,9 +4,9 @@ declare const SGWorld: ISGWorld;
 declare const SessionManager: ISessionManager;
 export const sgWorld = SGWorld;
 export const sessionManager = SessionManager;
-//export const basePath = "\\\\Skyline_NAS/Data/C-ARMSAS/TableAPI/Axiom/";
+export const basePath = "\\\\Skyline_NAS/Data/C-ARMSAS/TableAPI/Axiom/";
 //export const basePath = "C:/dev/Github/AxiomSkylineHolotable/dist/Axiom/";
-export const basePath = "C:/Users/Nicho/OneDrive - NGIS Australia Pty Ltd/Development/AxiomSkylineHolotable/dist/Axiom/";
+//export const basePath = "C:/Users/Nicho/OneDrive - NGIS Australia Pty Ltd/Development/AxiomSkylineHolotable/dist/Axiom/";
 
 if (document.readyState === 'complete')
   ProgramManager.getInstance().Init();
