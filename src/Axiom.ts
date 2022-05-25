7f20e60a7986166def72ff9848d86de7948e2556
Nicholas Chai <nicholas.chai@outook.com>
2022-05-26 09:24:40 +1000

    the path on the table got corrupted so this change now reflects the new path.
    the fly file project has been updated accordingly as well

--- src/Axiom.ts
+++ src/Axiom.ts
@@ -4,7 +4,7 @@ declare const SGWorld: ISGWorld;
 declare const SessionManager: ISessionManager;
 export const sgWorld = SGWorld;
 export const sessionManager = SessionManager;
-export const basePath = "\\\\Skyline_NAS/Data/C-ARMSAS/TableAPI/Axiom/";
+export const basePath = "\\\\Skyline_NAS/Data/C-ARMSAS/Axiom/";
 //export const basePath = "C:/dev/Github/AxiomSkylineHolotable/dist/Axiom/";
 //export const basePath = "C:/Users/Nicho/OneDrive - NGIS Australia Pty Ltd/Development/AxiomSkylineHolotable/dist/Axiom/";
 
