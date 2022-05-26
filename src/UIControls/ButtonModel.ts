bf57bc4be23d8c769d8440b16fca1d39845a60c0
David Wilton <dave_wilton@msn.com>
2022-05-26 18:17:39 +1000

    add handler

--- src/UIControls/ButtonModel.ts
+++ src/UIControls/ButtonModel.ts
@@ -8,32 +8,40 @@ export class ButtonModel extends Button {
   modelId: string = "";
 
   Draw() {
+    if (this.initError) return;
     const pos = roomToWorldCoord(this.roomPosition);
-    if (this.ID === undefined) {
-      const obj = sgWorld.Creator.CreateModel(pos, basePath + "/ui/blank.xpl2", this.scale, 0, this.groupID, this.name);
-      obj.Terrain.Tint = ProgramManager.getInstance().userModeManager!.getColorFromString("black", 100);
-      this.ID = obj.ID;
-      // create a model and float above
-      const model = sgWorld.Creator.CreateModel(pos, this.modelPath, this.scale, 0, this.groupID, this.name);
-      this.modelId = model.ID;
+    try {
 
-    } else {
-      // Move the button to be in the right spot
-      const obj: ITerrainModel = getItemById(this.ID) as ITerrainModel;
-      if(!obj) return;
-      obj.Position = pos;
-      obj.Position.Altitude =   obj.Position.Altitude
-      obj.ScaleFactor = this.scale * (ControllerReader.controllerInfos[1].scaleFactor ?? 1.5);
+      if (this.ID === undefined) {
+        const obj = sgWorld.Creator.CreateModel(pos, basePath + "/ui/blank.xpl2", this.scale, 0, this.groupID, this.name);
+        obj.Terrain.Tint = ProgramManager.getInstance().userModeManager!.getColorFromString("black", 100);
+        obj.Tooltip.Text = this.tooltip;
+        this.ID = obj.ID;
+        // create a model and float above
+        const model = sgWorld.Creator.CreateModel(pos, this.modelPath, this.scale, 0, this.groupID, this.name);
+        this.modelId = model.ID;
+        model.SetParam(200, 0x200);
 
-      const model: ITerrainModel = getItemById(this.modelId) as ITerrainModel;
-      if(!model) return;
-      model.Position = pos;
-      model.ScaleFactor = this.scale * (ControllerReader.controllerInfos[1].scaleFactor ?? 1.5);
-      if (this.color) {
-        // tint the model
-        model.Terrain.Tint = this.color;
+      } else {
+        // Move the button to be in the right spot
+        const obj: ITerrainModel = getItemById(this.ID) as ITerrainModel;
+        if (!obj) return;
+        obj.Position = pos;
+        obj.Position.Altitude = obj.Position.Altitude
+        obj.ScaleFactor = this.scale * (ControllerReader.controllerInfos[1].scaleFactor ?? 1.5);
+
+        const model: ITerrainModel = getItemById(this.modelId) as ITerrainModel;
+        if (!model) return;
+        model.Position = pos;
+        model.ScaleFactor = this.scale * (ControllerReader.controllerInfos[1].scaleFactor ?? 1.5);
+        if (this.color) {
+          // tint the model
+          model.Terrain.Tint = this.color;
+        }
+        // objLbl.Position.Pitch = 90
       }
-      // objLbl.Position.Pitch = 90
+    } catch (error) {
+        console.error("button error not found:: " + this.modelPath)
     }
   }
 
@@ -50,7 +58,7 @@ export class ButtonModel extends Button {
     ProgramManager.getInstance().deleteItemSafe(this.modelId)
   }
 
- 
+
 
 
 }
