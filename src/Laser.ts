713a21945399373b7773d34d038da542b5ae22c3
rwillsonAxiom <rwillson@axiomholographics.com>
2022-07-27 17:58:06 +1000

    work

--- src/Laser.ts
+++ src/Laser.ts
@@ -21,9 +21,10 @@ export class Laser {
   collision?: collisionInfo;
 
   UpdateTable(userIndex: number) {
-    const position = ControllerReader.controllerInfos[userIndex].wandPosition;
+    const position = ControllerReader.controllerInfos[userIndex].wandPosition?.Copy();
     if (position === undefined)
       return;
+    position.Distance *= 2;
     sgWorld.SetParam(8300, position); // Pick ray
     const hitObjectID = sgWorld.GetParam(8310) as string | undefined;
     let distToHitPoint = sgWorld.GetParam(8312) as number;    // Get distance to hit point
