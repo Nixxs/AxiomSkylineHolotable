f5b49efb7c9dcd21a7ac17ac207f542abaf0efe3
David Wilton <dave_wilton@msn.com>
2022-07-29 14:19:33 +1000

    init

---
+++ src/GpsTracking.ts
@@ -0,0 +1,17 @@
+
+
+/**
+ * Android app reports locations to an endpoint
+ *
+ * @class GpsTracking
+ */
+class GpsTracking {
+
+  jsonURL = "https://defense-3d-mission-default-rtdb.asia-southeast1.firebasedatabase.app/.json"
+
+  constructor() {
+
+  }
+
+
+}
