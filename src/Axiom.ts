e62ebb3005eb3fbaff2aced88cb3ef2a1d0f5a34
rwillsonAxiom <104418554+rwillsonAxiom@users.noreply.github.com>
2022-08-01 17:09:24 +1000

    Update src/Axiom.ts

--- src/Axiom.ts
+++ src/Axiom.ts
@@ -6,7 +6,7 @@ export const sgWorld = SGWorld;
 export const sessionManager = SessionManager;
 // export const basePath = "\\\\Skyline_NAS/Data/C-ARMSAS/Axiom/";
 // export const basePath = "C:/dev/Github/AxiomSkylineHolotable/dist/Axiom/";
-export const basePath = "T:/ReubenWillson/Axiom/";
+export const basePath = "c:\\Axiom\\DW\\dist\\axiom\\";
 
 if (document.readyState === 'complete')
   ProgramManager.getInstance().Init();
