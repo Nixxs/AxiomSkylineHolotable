2f86c8a72d9dd0920def482f80ef87e20d2871a9
Reuben Willson <rwillson@euclideonholographics.com>
2022-09-20 10:44:05 +1000

    fix basepath forever

--- src/Axiom.ts
+++ src/Axiom.ts
@@ -4,12 +4,45 @@ declare const SGWorld: ISGWorld;
 declare const SessionManager: ISessionManager;
 export const sgWorld = SGWorld;
 export const sessionManager = SessionManager;
-// export const basePath = "\\\\Skyline_NAS/Data/C-ARMSAS/Axiom/";
-// export const basePath = "C:/dev/Github/AxiomSkylineHolotable/dist/Axiom/";
-export const basePath = "c:\\Axiom\\DW\\dist\\axiom\\";
 
+function getBasePath() {
+  return new Promise<string>(go => {
+    const folder = window.location.pathname.match(/^[\/\\]*(.*[\/\\])/)?.[1];
+    if (folder === undefined) {
+      // Wait for folder
+      const div = document.createElement("div");
+      div.id = "folderInput";
+      const label = document.createElement("label")
+      const input = document.createElement("input");
+      const button = document.createElement("button");
 
-if (document.readyState === 'complete')
-  ProgramManager.getInstance().Init();
-else
-  window.addEventListener("load", ProgramManager.getInstance().Init);
+      label.textContent = "Please manually enter the folder";
+      alert("Please manually enter the folder");
+      input.focus();
+
+      button.textContent = "Confirm";
+
+      button.onclick = () => {
+        div.parentNode?.removeChild(div);
+
+        go(input.value);
+      };
+
+      div.appendChild(label);
+      label.appendChild(input);
+      div.appendChild(button);
+      document.body.appendChild(div);
+      return;
+    }
+    go(decodeURI(folder));
+  });
+}
+
+export let basePath = "";
+(async () => {
+  basePath = await getBasePath();
+  if (document.readyState === 'complete')
+    ProgramManager.getInstance().Init();
+  else
+    window.addEventListener("load", ProgramManager.getInstance().Init);
+})();
